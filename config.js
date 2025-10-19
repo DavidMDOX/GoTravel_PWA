51a79e8c3e1f91a0f4373176ea1eec83bf3a3e2d
DavidMDOX <ljm1994619@gmail.com>
2025-10-19 16:07:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 // 👉 Set this to your Hugging Face Space URL (the one that shows your app).
-window.HF_SPACE_URL = "https://huggingface.co/spaces/DavidMDOX/GoTravel_UIpro"; // e.g., https://my-cool-app.hf.space
-
+window.HF_SPACE_URL = "https://DavidMDOX-GoTravel_UIpro.hf.space"; // e.g., https://my-cool-app.hf.space
 
