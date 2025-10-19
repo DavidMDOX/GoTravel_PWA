d3720d526715554af62c3c4b9448769100b66d15
DavidMDOX <ljm1994619@gmail.com>
2025-10-19 21:31:57 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// 👉 Set this to your Hugging Face Space URL (the one that shows your app).
-window.HF_SPACE_URL = "https://davidmdox-gotravel-uipro.hf.space"; // e.g., https://my-cool-app.hf.space
-
+window.HF_SPACE_URL = "https://davidmdox-gotravel-uipro.hf.space";
